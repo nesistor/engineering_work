49e911d0dda78f13c2742f130dbf6ce2eda30c66
Karol <karolmalicki.001@gmail.com>
2024-10-31 02:38:21 +0100

    created Chat

--- frontend/src/components/Home/Chat.vue
+++ frontend/src/components/Home/Chat.vue
@@ -1,70 +1,73 @@
 <template>
-    <div class="page-background">
-      <div class="chat-container">
-        <!-- Application Header -->
-        <header class="header">
-          <h1>Llama Assistant</h1>
-          <p>Twój osobisty asystent do zadań specjalnych. Zapytaj, a uzyskasz odpowiedź!</p>
-        </header>
-  
-        <!-- Messages Container -->
-        <div class="messages-container">
-          <!-- Llama message with icon on the left -->
-          <div class="message llama-message">
-            <div class="icon">
-              <img src="@/assets/images/llama-icon.png" alt="Llama Icon" class="user-icon" />
-            </div>
-            <span class="message-content">
-              Hej, jestem osobistym asystentem pana Karola. Czego chciałbyś się dowiedzieć o jego umiejętnościach?
-            </span>
+  <div class="page-background">
+    <div class="chat-container">
+      <!-- Application Header -->
+      <header class="header">
+        <h1>Llama Assistant</h1>
+        <p>Twój osobisty asystent do zadań specjalnych. Zapytaj, a uzyskasz odpowiedź!</p>
+      </header>
+
+      <!-- Messages Container -->
+      <div class="messages-container">
+        <!-- Llama message with icon on the left -->
+        <div class="message llama-message">
+          <div class="icon">
+            <img src="@/assets/images/llama-icon.png" alt="Llama Icon" class="user-icon" />
           </div>
-  
-          <!-- User messages with SVG icon on the right -->
-          <div
-            class="message user-message"
-            v-for="(message, index) in messages"
-            :key="index"
-          >
-            <span class="message-content">{{ message }}</span>
-            <div class="icon">
-              <img src="@/assets/images/user-icon.svg" alt="User Icon" class="user-icon" />
-            </div>
-          </div>
+          <span class="message-content">
+            Hej, jestem osobistym asystentem pana Karola. Czego chciałbyś się dowiedzieć o jego umiejętnościach?
+          </span>
         </div>
-  
-        <!-- Input container for message entry -->
-        <div class="input-container">
-          <input
-            type="text"
-            v-model="newMessage"
-            @keyup.enter="sendMessage"
-            placeholder="Napisz wiadomość..."
-          />
-          <button @click="sendMessage">Send</button>
+
+        <!-- User messages with SVG icon on the right -->
+        <div
+          class="message user-message"
+          v-for="(message, index) in messages"
+          :key="index"
+        >
+          <span class="message-content">{{ message }}</span>
+          <div class="icon">
+            <img src="@/assets/images/user-icon.svg" alt="User Icon" class="user-icon" />
+          </div>
         </div>
       </div>
+
+      <!-- Input container for message entry -->
+      <div class="input-container">
+        <input
+          type="text"
+          v-model="newMessage"
+          @keyup.enter="sendMessage"
+          placeholder="Napisz wiadomość..."
+        />
+        <button @click="sendMessage">Wyślij</button>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import '../../assets/css/Chat.css';
-  
-  export default {
-    name: 'ChatApp',
-    data() {
-      return {
-        messages: [],
-        newMessage: '',
-      };
+  </div>
+</template>
+
+<script>
+import '../../assets/css/Chat.css';
+
+export default {
+  name: 'ChatApp',
+  data() {
+    return {
+      messages: [],
+      newMessage: '',
+    };
+  },
+  methods: {
+    sendMessage() {
+      if (this.newMessage.trim() !== '') {
+        this.messages.push(this.newMessage);
+        this.newMessage = '';
+        this.$nextTick(() => {
+          const messagesContainer = this.$el.querySelector('.messages-container');
+          messagesContainer.scrollTop = messagesContainer.scrollHeight; // Scroll to the bottom
+        });
+      }
     },
-    methods: {
-      sendMessage() {
-        if (this.newMessage.trim() !== '') {
-          this.messages.push(this.newMessage);
-          this.newMessage = '';
-        }
-      },
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
