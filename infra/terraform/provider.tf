79306683edd188e31f2ff5b0eb1d054f3124732e
Karol <karolmalicki.001@gmail.com>
2024-12-04 11:45:22 +0100

    created terraform key for terraform service-account with policies

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file(var.gcp_credentials_file) # Path to the JSON service account key file
-  project     = var.gcp_project               # GCP project ID
-  region      = var.gcp_region                # Region used in the project
-  zone        = var.gcp_zone                  # Zone used in the project
+  credentials = file(var.gcp_credentials_file) 
+  project     = var.gcp_project              
+  region      = var.gcp_region                
+  zone        = var.gcp_zone                  
 }
