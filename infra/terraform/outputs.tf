c06b2d1f088c28ba159b5e47263c7b9d4e9d2140
Karol <karolmalicki.001@gmail.com>
2024-12-04 11:11:53 +0100

    infra creted

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,14 +1,34 @@
 output "gke_cluster_name" {
-  description = "Name of the GKE cluster"
-  value       = google_container_cluster.gke_cluster.name
+  description = "The name of the GKE cluster"
+  value       = google_container_cluster.primary.name
 }
 
 output "gke_cluster_endpoint" {
   description = "The endpoint of the GKE cluster"
-  value       = google_container_cluster.gke_cluster.endpoint
+  value       = google_container_cluster.primary.endpoint
 }
 
-output "gke_cluster_ca_certificate" {
-  description = "The CA certificate of the GKE cluster"
-  value       = google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate
+output "gke_cluster_zone" {
+  description = "The zone of the GKE cluster"
+  value       = google_container_cluster.primary.zone
+}
+
+output "gcr_docker_registry" {
+  description = "The Docker Registry URL for the GCP project"
+  value       = "us-central1-docker.pkg.dev/${var.gcp_project}/my-microservices-repo"
+}
+
+output "kubernetes_namespace" {
+  description = "Namespace for microservices deployment"
+  value       = "microservices-app"
+}
+
+output "helm_release_name" {
+  description = "Name of the Helm release"
+  value       = "microservices"
+}
+
+output "service_account_email" {
+  description = "Email of the Google service account used"
+  value       = google_service_account.sa.email
 }
--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,6 +1,45 @@
 provider "google" {
-  credentials = file("<path_to_your_service_account_key_file>")
-  project     = var.gcp_project
-  region      = var.gcp_region
-  zone        = var.gcp_zone
+  credentials = file(var.gcp_credentials_file) # Path to the JSON service account key file
+  project     = var.gcp_project               # GCP project ID
+  region      = var.gcp_region                # Region used in the project
+  zone        = var.gcp_zone                  # Zone used in the project
+}
+
+variable "gcp_credentials_file" {
+  description = "Path to the JSON service account credentials file for GCP"
+  type        = string
+  default     = "<path_to_your_service_account_key_file>" # Replace with the actual path
+}
+
+variable "gcp_project" {
+  description = "Google Cloud project ID"
+  type        = string
+  default     = "my-microservices-app" # Your project
+}
+
+variable "gcp_region" {
+  description = "Region used in the Google Cloud project"
+  type        = string
+  default     = "us-central1" # Default region
+}
+
+variable "gcp_zone" {
+  description = "Zone used in the Google Cloud project"
+  type        = string
+  default     = "us-central1-a" # Default zone
+}
+
+output "gcp_project_id" {
+  description = "Google Cloud project ID"
+  value       = var.gcp_project
+}
+
+output "gcp_region" {
+  description = "Google Cloud project region"
+  value       = var.gcp_region
+}
+
+output "gcp_zone" {
+  description = "Google Cloud project zone"
+  value       = var.gcp_zone
 }
